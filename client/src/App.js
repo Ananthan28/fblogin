4960a777e886db0033a3d042498e478ebcff8d1a
ananthan28 <souparnikaananthan28@gmail.com>
2023-10-12 11:48:16 +0530

    button

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import MyButton from './component/button';
 
 function App() {
   return (
-   <div>
+    <div>
+      <div class="top">
+    <img src="https://static.xx.fbcdn.net/rsrc.php/yI/r/4aAhOWlwaXf.svg"></img>
     <form>
     <fieldset>
-    <h1>
-      facebook 
-    </h1>
-    <input class = "feild" type="text" id="username" name="Username" Placeholder="Username"></input><br></br>
+      <p>Log in to Facebook</p>
+    <input class = "feild" type="text" id="username" name="Email address or Phone number" Placeholder="Email address or Phone number"></input><br></br>
     
     <br></br>
     <input class = "feild" type="text" id="pwd" name="Password" Placeholder="Password"></input><br></br><br></br>
 
     <input class ="login" type="button" value="login"></input><br></br><br></br>
 
-    <a class="fpassword" href="">forgot your Password</a>
+    <a class="fpassword" href="">forgot your Password? .</a> &nbsp;
     <a class="sfacebook" href="">signup for facebook</a>
     </fieldset>
     </form>
+    </div>
+    <div>
+      
+    </div>
    </div>
   );
 }
---
+++ client/src/component/button.js
@@ -0,0 +1,5 @@
+import React from "react";
+function MyButton(){
+    return <button>Log In</button>
+}
+export default MyButton;
